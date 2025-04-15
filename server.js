b91286146daf97eae8147054845e858770b68539
Eduardo-Muricy <Eduardo-Muricy@hotmail.com>
2025-04-15 19:10:03 -0300

    1

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
-app.use(cors("http://localhost:5173"))
+app.use(cors("http://localhost:5173", "https://toodo-liist.netlify.app"))
 
 
 
